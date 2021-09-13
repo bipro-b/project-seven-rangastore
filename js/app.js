54c8217d0fa4a235157f0cafef9a0e63bea37797
Bipro <biprobarai18@gmail.com>
2021-09-13 10:05:37 +0600

    last commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`; //https://fakestoreapi.com/products
+  const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
